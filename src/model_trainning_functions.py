0180381b3f6578aa4c8c2a1aad45a01de53689a3
dcuoliveira <dan.cunhaa@gmail.com>
2022-04-16 22:30:35 -0300

    first version of model trainning

---
+++ src/model_trainning_functions.py
@@ -0,0 +1,111 @@
+import os
+import pandas as pd
+from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, KFold
+
+
+def hyper_params_search(df,
+                        wrapper,
+                        n_iter,
+                        n_splits,
+                        n_jobs,
+                        verbose,
+                        seed,
+                        target_name):
+    """
+    Use the dataframe 'df' to search for the best
+    params for the model 'wrapper'.
+    The CV split is performed using the TimeSeriesSplit
+    class.
+    We can define the size of the test set using the formula
+    ``n_samples//(n_splits + 1)``,
+    where ``n_samples`` is the number of samples. Hence,
+    we can define
+    n_splits = (n - test_size) // test_size
+    :param df: train data
+    :type df: pd.DataFrame
+    :param wrapper: predictive model
+    :type wrapper: sklearn model wrapper
+    :param n_iter: number of hyperparameter searchs
+    :type n_iter: int
+    :param n_splits: number of splits for the cross-validation
+    :type n_splits: int
+    :param n_jobs: number of concurrent workers
+    :type n_jobs: int
+    :param verbose: param to print iteration status
+    :type verbose: bool, int
+    :param target_name: name of the target column in 'df'
+    :type target_name: str
+    :return: R2 value
+    :rtype: float
+    """
+
+    wrapper = wrapper()
+
+    X = df.drop(target_name, 1).values
+    y = df[target_name].values
+
+    cv_splits = KFold(n_splits=n_splits)
+    mse_scorer = make_scorer(mean_squared_error)
+
+    if wrapper.search_type == 'random':
+        model_search = RandomizedSearchCV(estimator=wrapper.ModelClass,
+                                          param_distributions=wrapper.param_grid,
+                                          n_iter=n_iter,
+                                          cv=cv_splits,
+                                          verbose=verbose,
+                                          n_jobs=n_jobs,
+                                          scoring=mse_scorer,
+                                          random_state=seed)
+    elif wrapper.search_type == 'grid':
+        model_search = GridSearchCV(estimator=wrapper.ModelClass,
+                                    param_grid=wrapper.param_grid,
+                                    cv=cv_splits,
+                                    verbose=verbose,
+                                    n_jobs=n_jobs,
+                                    scoring=mse_scorer)
+    else:
+        raise Exception('search type method not registered')
+
+    model_search = model_search.fit(X, y)
+
+    return model_search
+
+
+def run_train_test_setup(target_name,
+                         inputs_path,
+                         outputs_path,
+                         dataset_names,
+                         wrapper,
+                         n_jobs,
+                         n_splits,
+                         n_iter,
+                         seed,
+                         verbose):
+
+    for dir_name in os.listdir(inputs_path):
+        for d_name in dataset_names:
+            train_data = pd.read_csv(os.path.join(inputs_path, dir_name, d_name + ".csv"))
+            train_data.set_index(["Var1", "Var2"], inplace=True)
+
+            test_data = pd.read_csv(os.path.join(inputs_path, dir_name, d_name + "_test.csv"))
+            test_data.set_index(["Var1", "Var2"], inplace=True)
+
+            model_search = hyper_params_search(df=train_data,
+                                               target_name=target_name,
+                                               wrapper=wrapper,
+                                               n_jobs=n_jobs,
+                                               n_splits=n_splits,
+                                               n_iter=n_iter,
+                                               seed=seed,
+                                               verbose=verbose)
+
+            X_test = test_data.drop(target_name, 1).values
+            test_pred = model_search.best_estimator_.predict(X_test)
+
+            test_data['prediction'] = test_pred
+
+            if not os.path.isdir(os.path.join(outputs_path, dir_name)):
+                os.mkdir(os.path.join(outputs_path, dir_name))
+
+            test_data.reset_index().to_csv(os.path.join(outputs_path, dir_name, d_name + "_result.csv"))
---
+++ src/run_ffnn.py
@@ -0,0 +1,27 @@
+import os
+
+from src.model_trainning_functions import run_train_test_setup
+from src.models import NNCombWrapper
+
+N_JOBS = 2  # number of jobs to run in parallel
+N_SPLITS = 10  # number of splits (k) to be made within the k fold cv
+N_ITER = 100  # number of parameter settings that are sampled
+SEED = 2294
+VERBOSE = True
+INPUTS_PATH = os.path.join(os.getcwd(), "data", "inputs")
+OUTPUTS_PATH = os.path.join(os.getcwd(), "data", "outputs")
+DATASET_NAMES = ["betadgp_covdgp_data", "betadgp_beta2x2_data", "betadgp_data"]
+TARGET_NAME = "betas_dgp"
+
+
+if __name__ == '__main__':
+    results = run_train_test_setup(inputs_path=INPUTS_PATH,
+                                   outputs_path=OUTPUTS_PATH,
+                                   target_name=TARGET_NAME,
+                                   dataset_names=DATASET_NAMES,
+                                   wrapper=NNCombWrapper,
+                                   n_jobs=N_JOBS,
+                                   n_splits=N_SPLITS,
+                                   n_iter=N_ITER,
+                                   seed=SEED,
+                                   verbose=VERBOSE)
