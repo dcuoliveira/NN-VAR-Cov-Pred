22147a161d8f1c2d2a3b93d2a3ebda8eb570d776
dcuoliveira <dan.cunhaa@gmail.com>
2022-07-02 21:23:37 -0300

    add train size

--- src/run_linear.py
+++ src/run_linear.py
@@ -1,19 +1,20 @@
 import os
 
 from src.training.runners import run_model_training
-from src.models.neural_networks import NNCombWrapper
+from src.models.linear import LinearReWrapper
 
-N_JOBS = 2  # number of jobs to run in parallel
-N_SPLITS = 10  # number of splits (k) to be made within the k fold cv
-N_ITER = 10  # number of parameter settings that are sampled
+N_JOBS = None  # number of jobs to run in parallel
+N_SPLITS = None  # number of splits (k) to be made within the k fold cv
+N_ITER = None  # number of parameter settings that are sampled
 SEED = 2294
 VERBOSE = True
 INPUTS_PATH = os.path.join(os.getcwd(), "data", "inputs")
 OUTPUTS_PATH = os.path.join(os.getcwd(), "data", "outputs")
 DATASET_NAMES = ["betadgp_covdgp_data", "betadgp_beta2x2_data", "betadgp_data"]
 TARGET_NAME = "betas_dgp"
-MODEL_TAG = "FFNN"
+MODEL_TAG = "linear_reg"
 STANDARDIZE = True
+TRAIN_SIZE = 0.7
 
 if __name__ == '__main__':
     results = run_model_training(inputs_path=INPUTS_PATH,
@@ -22,7 +23,8 @@ if __name__ == '__main__':
                                  dataset_names=DATASET_NAMES,
                                  model_tag=MODEL_TAG,
                                  standardize=STANDARDIZE,
-                                 wrapper=NNCombWrapper,
+                                 train_size=TRAIN_SIZE,
+                                 wrapper=LinearReWrapper,
                                  n_jobs=N_JOBS,
                                  n_splits=N_SPLITS,
                                  n_iter=N_ITER,
