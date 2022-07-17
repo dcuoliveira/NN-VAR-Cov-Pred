7163b414899e03b00965523816a835233c566999
dcuoliveira <dan.cunhaa@gmail.com>
2022-07-17 15:40:03 -0300

    add save and load pickle

---
+++ src/utils/Pyutils.py
@@ -0,0 +1,26 @@
+import pickle
+
+def save_pkl(data,
+             path):
+
+    with open(path, 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_pkl(path):
+
+    with open(path, 'rb') as handle:
+        data = pickle.load(handle)
+    return data
+
+DEBUG = False
+
+if __name__ == '__main__':
+
+    if DEBUG:
+        import os
+
+        data = {"a": 1, "b": 2}
+        save_pkl(data=data,
+                 path=os.path.join(os.getcwd(), "test.pickle"))
+
+        new_data = load_pkl(path=os.path.join(os.getcwd(), "test.pickle"))
