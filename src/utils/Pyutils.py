2ec9ba41d935f81c721e0054c8ddc938031f0407
dcuoliveira <dan.cunhaa@gmail.com>
2022-07-19 09:49:46 -0300

    add close

--- src/utils/Pyutils.py
+++ src/utils/Pyutils.py
@@ -5,14 +5,16 @@ def save_pkl(data,
 
     with open(path, 'wb') as handle:
         pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        handle.close()
 
 def load_pkl(path):
 
     with open(path, 'rb') as handle:
         data = pickle.load(handle)
+        handle.close()
     return data
 
-DEBUG = False
+DEBUG = True
 
 if __name__ == '__main__':
 
