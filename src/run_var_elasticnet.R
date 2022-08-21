00126b1d439dcbf40af8148d3c28c4aea26d3b87
dcuoliveira <dan.cunhaa@gmail.com>
2022-08-21 13:18:53 -0300

    chechekpoint var enet

--- src/run_var_elasticnet.R
+++ src/run_var_elasticnet.R
@@ -1,17 +1,21 @@
 
+source(file.path(getwd(), "src", "models", "penalized_var.R"))
+source(file.path(getwd(), "src", "training", "Runners.R"))
 
 N_SPLITS <- 10  # number of splits (k) to be made within the k fold cv
 N_ITER <- 50  # number of parameter settings that are sampled
 SEED <- 2294
-VERBOSE <- TRUE
 INPUTS_PATH <- file.path(getwd(), "src", "data", "inputs")
 OUTPUTS_PATH <- file.path(getwd(), "data", "outputs")
-DATASET_NAMES <- c("betadgp_covdgp_data", "betadgp_beta2x2_data", "betadgp_data")
+DATASET_NAMES <- c("data_dgp")
 TARGET_NAME <- "betas_dgp"
 MODEL_TAG <- "var_enet"
 STANDARDIZE <- TRUE
-TRAIN_SIZE <- 0.7
 OUTPUT_OVRD <- TRUE
+
+CV_TYPE <- "Rolling" # rolling cv type
+TRAIN_SIZE <- 0.7 # stands for the window.size parameter on the BigVAR function
+VERBOSE <- TRUE
 
 results <- run_model_training(inputs_path=INPUTS_PATH,
                               outputs_path=OUTPUTS_PATH,
@@ -20,7 +24,7 @@ results <- run_model_training(inputs_path=INPUTS_PATH,
                               model_tag=MODEL_TAG,
                               standardize=STANDARDIZE,
                               train_size=TRAIN_SIZE,
-                              wrapper=FFNNWrapper,
+                              wrapper=EnetVARWrapper(),
                               n_jobs=N_JOBS,
                               n_splits=N_SPLITS,
                               n_iter=N_ITER,
