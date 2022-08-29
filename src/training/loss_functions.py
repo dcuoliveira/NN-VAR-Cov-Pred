a1c8cc4f0feb55d5ada3232b855c3a8a05152877
dcuoliveira <dan.cunhaa@gmail.com>
2022-08-29 20:43:26 -0300

    add comment

--- src/training/loss_functions.py
+++ src/training/loss_functions.py
@@ -3,7 +3,9 @@ import tensorflow as tf
 def weighted_mean_squared_error(y_true,
                                 y_pred,
                                 w=0.1):
-    y_pred_of_nonzeros = tf.where(tf.equal(y_true, 0), y_pred - y_pred, y_pred)
+    y_pred_of_nonzeros = tf.where(tf.equal(y_true, 0),
+                                  y_pred - y_pred,
+                                  y_pred)
 
-    # non-zero target predictions are penalized less than the average prediction
+    # if y_true is non-zero and y_pred is zero => this error will cost the model (w*100)% more than any other error
     return tf.reduce_mean(tf.square(y_true - y_pred_of_nonzeros)) + tf.reduce_mean(tf.square(y_true - y_pred)) * w
