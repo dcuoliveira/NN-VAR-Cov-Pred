a1308513a783a9a16fd2e3f07207ec0668cb5c29
dcuoliveira <dan.cunhaa@gmail.com>
2022-09-26 21:31:16 -0300

    refactor name

--- src/run_dnn.py
+++ src/run_dnn.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
                                  model_tag=MODEL_TAG,
                                  standardize=STANDARDIZE,
                                  train_size=TRAIN_SIZE,
-                                 wrapper=FFNNWrapper,
+                                 wrapper=DNN1Wrapper,
                                  n_jobs=N_JOBS,
                                  n_splits=N_SPLITS,
                                  n_iter=N_ITER,
