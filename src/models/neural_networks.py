d7b43add20385f94b029e2411ba0b8340101320d
dcuoliveira <dan.cunhaa@gmail.com>
2022-09-28 13:12:51 -0300

    add nn with none parameters

--- src/models/neural_networks.py
+++ src/models/neural_networks.py
@@ -80,3 +80,21 @@ class DNN1Wrapper():
 
         if model_params is not None:
             self.param_grid.update(model_params)
+
+
+class FFNNFixedWrapper():
+    def __init__(self, model_params=None):
+        self.model_name = "ffnn"
+        self.search_type = 'random'
+        self.param_grid = {"n_hidden": None,
+                           "n_neurons": None,
+                           "learning_rate": reciprocal(3e-4, 3e-2),
+                           "activation": ["relu"],
+                           "loss_name": ["mse"]}
+        self.epochs = 50
+        self.callbacks = [keras.callbacks.EarlyStopping(patience=10)]
+
+        self.ModelClass = keras.wrappers.scikit_learn.KerasRegressor(FFNN)
+
+        if model_params is not None:
+            self.param_grid.update(model_params)
