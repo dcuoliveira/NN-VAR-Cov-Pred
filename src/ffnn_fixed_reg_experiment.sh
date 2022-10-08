34424137521b9d081ff39872cea56bc29f1a96c5
dcuoliveira <dan.cunhaa@gmail.com>
2022-10-08 16:35:01 -0300

    delete screen function

--- src/ffnn_fixed_reg_experiment.sh
+++ src/ffnn_fixed_reg_experiment.sh
@@ -1,71 +1,71 @@
-screen python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 1 1000 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 1 10000 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 5 1000 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 5 10000 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 10 100 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 10 1000 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 10 10000 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 15 100 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 15 1000 relu mse 10 50
 
-screen python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 10
-screen python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 20
-screen python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 30
-screen python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 40
-screen python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 10
+python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 20
+python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 30
+python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 40
+python3 run_ffnn_fixed_reg.py 15 10000 relu mse 10 50
