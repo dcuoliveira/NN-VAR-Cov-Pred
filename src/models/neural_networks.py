b0957d7f2a2a1c98fdb5c9838a8f7c05cbe5985f
dcuoliveira <dan.cunhaa@gmail.com>
2022-11-18 07:38:08 -0300

    simplify

--- src/models/neural_networks.py
+++ src/models/neural_networks.py
@@ -73,43 +73,6 @@ class FFNNWrapper():
         if model_params is not None:
             self.param_grid.update(model_params)
 
-
-class FFNNWrapperWMSE():
-    def __init__(self, model_params=None):
-        self.model_name = "ffnn"
-        self.search_type = 'random'
-        self.param_grid = {"n_hidden": np.arange(1, 10+1),
-                           "n_neurons": np.arange(1, 100+1),
-                           "learning_rate": list(1e-3 * (10 ** (np.arange(100) / 30))),
-                           "activation": ["relu"],
-                           "loss_name": ["wmse"]}
-        self.epochs = 100
-        self.callbacks = [keras.callbacks.EarlyStopping(patience=25)]
-
-        self.ModelClass = keras.wrappers.scikit_learn.KerasRegressor(FFNN, verbose=0)
-
-        if model_params is not None:
-            self.param_grid.update(model_params)
-
-
-class DNN1Wrapper():
-    def __init__(self, model_params=None):
-        self.model_name = "ffnn"
-        self.search_type = 'random'
-        self.param_grid = {"n_hidden": np.arange(1, 1000+1),
-                           "n_neurons": np.arange(1, 1000+1),
-                           "learning_rate": list(1e-3 * (10 ** (np.arange(100) / 30))),
-                           "activation": ["relu"],
-                           "loss_name": ["mse"]}
-        self.epochs = 100
-        self.callbacks = [keras.callbacks.EarlyStopping(patience=25)]
-
-        self.ModelClass = keras.wrappers.scikit_learn.KerasRegressor(FFNN, verbose=0)
-
-        if model_params is not None:
-            self.param_grid.update(model_params)
-
-
 class FFNNFixedWrapper():
     def __init__(self, model_params=None):
         self.model_name = "ffnn"
