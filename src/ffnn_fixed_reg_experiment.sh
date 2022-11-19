1a39dddb5fad518898c0d12f9e5f1d06ef1c520f
dcuoliveira <dan.cunhaa@gmail.com>
2022-11-19 19:41:51 -0300

    change activation function

--- src/ffnn_fixed_reg_experiment.sh
+++ src/ffnn_fixed_reg_experiment.sh
@@ -1,6 +1,6 @@
-python3 run_ffnn_fixed_reg.py 1 100 relu mse 10 50
-python3 run_ffnn_fixed_reg.py 2 100 relu mse 10 50
-python3 run_ffnn_fixed_reg.py 3 100 relu mse 10 50
-python3 run_ffnn_fixed_reg.py 4 100 relu mse 10 50
-python3 run_ffnn_fixed_reg.py 5 100 relu mse 10 50
+python3 run_ffnn_fixed_reg.py 1 100 swish mse 10 50
+python3 run_ffnn_fixed_reg.py 2 100 swish mse 10 50
+python3 run_ffnn_fixed_reg.py 3 100 swish mse 10 50
+python3 run_ffnn_fixed_reg.py 4 100 swish mse 10 50
+python3 run_ffnn_fixed_reg.py 5 100 swish mse 10 50
 
