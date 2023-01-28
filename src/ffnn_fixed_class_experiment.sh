d03faed9005ae705b0df7a1c87a6810283387329
dcuoliveira <dan.cunhaa@gmail.com>
2023-01-28 19:37:14 -0300

    delete

--- src/ffnn_fixed_class_experiment.sh
+++
@@ -1,6 +0,0 @@
-python3 run_ffnn_fixed_class.py 1 100 swish binaryx 10 50
-python3 run_ffnn_fixed_class.py 2 100 swish binaryx 10 50
-python3 run_ffnn_fixed_class.py 3 100 swish binaryx 10 50
-python3 run_ffnn_fixed_class.py 4 100 swish binaryx 10 50
-python3 run_ffnn_fixed_class.py 5 100 swish binaryx 10 50
-
