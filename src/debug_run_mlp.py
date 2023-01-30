d84d8fa99381f521b922e9bb184485db383f80c8
dcuoliveira <dan.cunhaa@gmail.com>
2023-01-29 21:37:55 -0300

    add debug parameters

--- src/debug_run_mlp.py
+++ src/debug_run_mlp.py
@@ -1,38 +1,40 @@
 import os
+import torch
 
-from training.runners import run_model_training
+from training.runners import run_new_model_training
 from models.neural_networks import MLPWrapper
 
-N_JOBS = -1  # number of jobs to run in parallel
+N_JOBS = 1  # number of jobs to run in parallel
 N_SPLITS = 5  # number of splits (k) to be made within the k fold cv
 N_ITER = 20  # number of parameter settings that are sampled
 SEED = 2294
 VERBOSE = False
-INPUTS_PATH = os.path.join(os.getcwd(), "data", "inputs")
-OUTPUTS_PATH = os.path.join(os.getcwd(), "data", "outputs")
-LOG_PATH = os.path.join(os.getcwd(), "data", "log")
+INPUTS_PATH = os.path.join(os.getcwd(), "src", "data", "inputs")
+OUTPUTS_PATH = os.path.join(os.getcwd(), "src", "data", "outputs")
+LOG_PATH = os.path.join(os.getcwd(), "src", "data", "log")
 DATASET_NAMES = ["betadgp_corrdgp_data", "betadgp_covdgp_data", "betadgp_beta2x2_data"]
 TARGET_NAME = "betas_dgp"
 MODEL_TAG = "mlp"
 STANDARDIZE = True
 TRAIN_SIZE = 0.7
 OUTPUT_OVRD = True
+CRITERION = torch.nn.MSELoss()
 
 if __name__ == '__main__':
-    results = run_model_training(inputs_path=INPUTS_PATH,
-                                 outputs_path=OUTPUTS_PATH,
-                                 log_path=LOG_PATH,
-                                 target_name=TARGET_NAME,
-                                 dataset_names=DATASET_NAMES,
-                                 model_tag=MODEL_TAG,
-                                 standardize=STANDARDIZE,
-                                 train_size=TRAIN_SIZE,
-                                 wrapper=MLPWrapper,
-                                 wrapper_ovrd=None,
-                                 n_jobs=N_JOBS,
-                                 n_splits=N_SPLITS,
-                                 n_iter=N_ITER,
-                                 dir_name_ovrd=["simple_ar"],
-                                 seed=SEED,
-                                 verbose=VERBOSE,
-                                 output_ovrd=OUTPUT_OVRD)
+    results = run_new_model_training(inputs_path=INPUTS_PATH,
+                                     outputs_path=OUTPUTS_PATH,
+                                     log_path=LOG_PATH,
+                                     target_name=TARGET_NAME,
+                                     dataset_names=DATASET_NAMES,
+                                     model_tag=MODEL_TAG,
+                                     standardize=STANDARDIZE,
+                                     train_size=TRAIN_SIZE,
+                                     wrapper=MLPWrapper,
+                                     criterion=CRITERION,
+                                     n_jobs=N_JOBS,
+                                     n_splits=N_SPLITS,
+                                     n_iter=N_ITER,
+                                     dir_name_ovrd=["simple_ar"],
+                                     seed=SEED,
+                                     verbose=VERBOSE,
+                                     output_ovrd=OUTPUT_OVRD)
