f8e32373771c249068c706d92318b965ec24730c
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-05 12:07:51 -0300

    fix dgp simulation

--- src/gen_multivariate_tsp.R
+++ src/gen_multivariate_tsp.R
@@ -34,45 +34,38 @@ foreach(model = MODELS)  %dopar% {
 
   for (k in seq(K_INIT, K, by = STEP)){
     for (network in NETWORKS){
+
        output_name <- paste0(model, "_", k, "_", network)
        new_folder <- file.path(OUTPUT_PATH, output_name)
        dir.create(new_folder)
 
-       mts <- sim.data(model = "ar2",
+       mts <- sim.data(model = model,
                       time = 1,
                       n.obs = N,
-                      n.var = 2,
+                      n.var = k,
                       seed = SEED,
                       prob0 = 0.9,
                       network = network)
+        p <- as.integer(str_split(model, "ar")[[1]][2])
 
        # simulated time series
        dgp_data <- mts$data1 %>% longitudinal_to_data.table()
-       fwrite(x = beta,
-              file = file.path(new_folder, "data_dgp.csv"),
+       fwrite(x = dgp_data,
+              file = file.path(new_folder, "data_dgp_train.csv"),
               row.names = FALSE)
-
-       # lagged dgp
-       lag_dgp_data <- dgp_data
-       for (p in 1:as.integer(str_split(model, "ar")[[1]][2])){
-              for (col in colnames(dgp_data)){
-                     col_number <- as.integer(str_split(colnames(lag_dgp_data)[dim(lag_dgp_data)[2]], "V")[[1]][2]) # nolint
-                     lag_dgp_data[[paste0("V", col_number + 1)]] <- lag(lag_dgp_data[[paste0("V", col_number)]], p) # nolint
-              }
-       }
 
        # dgp covariance matrix ?
        sigma <- mts$sigma %>% as.data.table()
        colnames(sigma) <- gsub("V", "", colnames(sigma))
        fwrite(x = sigma,
-              file = file.path(new_folder, "sigma.csv"),
+              file = file.path(new_folder, "sigma_train.csv"),
               row.names = FALSE)
 
        # dgp betas
        beta <- mts$gamma %>% t() %>% as.data.table()
        colnames(beta) <- gsub("V", "", colnames(beta))
        fwrite(x = beta,
-              file = file.path(new_folder, "beta.csv"),
+              file = file.path(new_folder, "beta_train.csv"),
               row.names = FALSE)
 
        # sample covariance matrix
@@ -81,46 +74,36 @@ foreach(model = MODELS)  %dopar% {
          dplyr::select(eq, everything())
        y_dgp <- melt(betas_dgp, id = c("eq")) %>%
         as.data.table() %>%
-         rename(betas_dgp = value)
-       cov_dgp <- cov(lag_dgp_data %>% tidyr::drop_na()) %>%
-        melt() %>%
-          as.data.table() %>%
-           rename(eq = Var1, variable = Var2, cov_dgp = value)
-       cov_dgp$eq <- gsub("V", "", cov_dgp$eq)
-       cov_dgp$variable <- gsub("V", "", cov_dgp$variable)
+         rename(betas_dgp = value) %>%
+          mutate(eq = as.character(eq), variable=as.character(variable))
+       cov_dgp <- cov_combination_2x2(data = dgp_data, p = p)
        betadgp_covdgp_data <- merge(y_dgp, cov_dgp)
        fwrite(x = betadgp_covdgp_data,
-              file = file.path(new_folder, "betadgp_covdgp_data.csv"),
+              file = file.path(new_folder, "betadgp_covdgp_data_train.csv"),
               row.names = FALSE)
 
        # sample correlation matrix
-       corr_dgp <- cor(lag_dgp_data %>% tidyr::drop_na()) %>%
-        melt() %>%
-          as.data.table() %>%
-           rename(eq = Var1, variable = Var2, cov_dgp = value)
-       corr_dgp$eq <- gsub("V", "", corr_dgp$eq)
-       corr_dgp$variable <- gsub("V", "", corr_dgp$variable)
+       corr_dgp <- corr_combination_2x2(data = dgp_data, p = p)
        betadgp_corrdgp_data <- merge(y_dgp, corr_dgp)
        fwrite(x = betadgp_corrdgp_data,
-              file = file.path(new_folder, "betadgp_corrdgp_data.csv"),
+              file = file.path(new_folder, "betadgp_corrdgp_data_train.csv"),
               row.names = FALSE)
 
        # beta 2x2 of each time series and lags
-       beta2x2_data <- lm_combination_2x2(data = dgp_data, p = 2) %>% # nolint
-        rename(eq = Var1, variable = Var2) %>%
-         mutate(eq = as.character(eq), variable = as.character(variable))
+       beta2x2_data <- lm_combination_2x2(data = dgp_data, p = p)
        betadgp_beta2x2_data <- merge(y_dgp, beta2x2_data)
        fwrite(x = betadgp_covdgp_data,
-              file = file.path(new_folder, "betadgp_beta2x2_data.csv"),
+              file = file.path(new_folder, "betadgp_beta2x2_data_train.csv"),
               row.names = FALSE)
 
        # all covariates
-       betadgp_data <- merge(betadgp_covdgp_data,
-                             betadgp_corrdgp_data,
-                             beta2x2_data,
-                             by = c("eq", "variable"))
+       betadgp_data <- merge(merge(betadgp_covdgp_data,
+                                   corr_dgp,
+                                   by = c("eq", "variable")),
+                            beta2x2_data,
+                            by = c("eq", "variable"))
        fwrite(x = betadgp_data,
-              file = file.path(new_folder, "betadgp_data.csv"),
+              file = file.path(new_folder, "betadgp_data_train.csv"),
               row.names = FALSE)
 
        # test data
@@ -131,35 +114,47 @@ foreach(model = MODELS)  %dopar% {
                            prob0 = PROB_OF_CONNECTION,
                            network = network)
        betas_dgp_test <- mts_test$gamma
+
        y_dgp_test <- melt(betas_dgp_test) %>%
         as.data.table() %>%
-        rename(betas_dgp = value)
-       cov_dgp_test <- melt(mts_test$sigma) %>%
-        as.data.table() %>%
-         rename(cov_dgp = value)
-       betadgp_covdgp_data_test <- merge(y_dgp_test, cov_dgp_test) %>%
-        filter((Var1 != Var2))
+        rename(eq = Var1, variable = Var2, betas_dgp = value) %>%
+         mutate(eq = as.character(eq), variable = as.character(variable))
+
+       dgp_data_test <- mts_test$data1 %>%
+        longitudinal_to_data.table()
+       fwrite(x = dgp_data_test,
+              file = file.path(new_folder, "data_dgp_test.csv"),
+              row.names = FALSE)
+
+        # test covariance estimate
+       cov_dgp_test <- cov_combination_2x2(data = dgp_data_test, p = p)
+       betadgp_covdgp_data_test <- merge(y_dgp_test, cov_dgp_test)
        fwrite(x = betadgp_covdgp_data_test,
               file = file.path(new_folder, "betadgp_covdgp_data_test.csv"),
               row.names = FALSE)
-       simulation_dgp_test <- mts_test$data1 %>% longitudinal_to_data.table()
-       beta2x2_data_test <- lm_combination_2x2(data = simulation_dgp_test)
-       betadgp_beta2x2_data_test <- merge(y_dgp_test, beta2x2_data_test) %>%
-        filter((Var1 != Var2))
+
+        # test covariance estimate
+       corr_dgp_test <- corr_combination_2x2(data = dgp_data_test, p = p)
+       betadgp_corrdgp_data_test <- merge(y_dgp_test, corr_dgp_test)
+       fwrite(x = betadgp_corrdgp_data_test,
+              file = file.path(new_folder, "betadgp_corrdgp_data_test.csv"),
+              row.names = FALSE)
+
+       beta2x2_data_test <- lm_combination_2x2(data = dgp_data_test, p = p)
+       betadgp_beta2x2_data_test <- merge(y_dgp_test, beta2x2_data_test)
        fwrite(x = betadgp_beta2x2_data_test,
               file = file.path(new_folder, "betadgp_beta2x2_data_test.csv"),
               row.names = FALSE)
-       betadgp_data_test <- merge(betadgp_covdgp_data_test,
-                                  betadgp_beta2x2_data_test %>%
-                                   select(Var1, Var2, beta_2x2)) %>%
-                                    filter((Var1 != Var2))
+
+       betadgp_data_test <- merge(merge(betadgp_covdgp_data_test,
+                                        corr_dgp_test,
+                                        by = c("eq", "variable")),
+                                 beta2x2_data_test,
+                                 by = c("eq", "variable"))
        fwrite(x = betadgp_data_test,
               file = file.path(new_folder, "betadgp_data_test.csv"),
               row.names = FALSE)
-       test_dgp_data <- mts_test$data1 %>% longitudinal_to_data.table()
-       fwrite(x = test_dgp_data,
-              file = file.path(new_folder, "data_dgp_test.csv"),
-              row.names = FALSE)
+
     }
   }
 }
