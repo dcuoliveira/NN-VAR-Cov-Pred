f3f4fd3d47a449c34fc732f905bc5698a8aa668c
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-23 15:43:04 -0300

    mean

--- src/training/loss_functions.py
+++ src/training/loss_functions.py
@@ -1,9 +1,9 @@
 import torch
 
-def WMSELoss(pred, true, w=0.5):
+def WMSELoss(pred, true, w=0.1):
     error = torch.square(true - pred)
     
     weights = (torch.where((true != 0) & (pred == 0), 1, 0) * w) + 1
     weighted_error = error * weights
 
-    return torch.sum(weighted_error)
+    return torch.mean(weighted_error)
