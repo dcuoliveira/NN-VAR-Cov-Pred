80d48e214a01158fbf10d7065371c54b86e35ccd
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-23 07:31:28 -0300

    add wmse

--- src/training/loss_functions.py
+++ src/training/loss_functions.py
@@ -1,14 +1,9 @@
-import tensorflow as tf
+import torch
 
-def swish(x):
-    return (tf.keras.backend.sigmoid(x) * x)
+def WMSELoss(pred, true, w=0.5):
+    error = torch.square(true - pred)
+    
+    weights = (torch.where((true != 0) & (pred == 0), 1, 0) * w) + 1
+    weighted_error = error * weights
 
-def weighted_mean_squared_error(y_true,
-                                y_pred,
-                                w=0.1):
-    y_pred_of_nonzeros = tf.where(tf.equal(y_true, 0),
-                                  tf.subtract(y_pred, y_pred),
-                                  y_pred)
-
-    # if y_true is non-zero and y_pred is zero => this error will cost the model (w*100)% more than any other error
-    return tf.reduce_mean(tf.square(y_true - y_pred_of_nonzeros)) + tf.reduce_mean(tf.square(y_true - y_pred)) * w
+    return torch.sum(weighted_error)
