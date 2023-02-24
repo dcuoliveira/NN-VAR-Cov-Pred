3403a579a334dc282a2b90c7b04db0b85bd23cd6
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-24 19:16:14 -0300

    fix weight

--- src/training/loss_functions.py
+++ src/training/loss_functions.py
@@ -1,6 +1,6 @@
 import torch
 
-def WMSELoss(pred, true, w=0.1):
+def WMSELoss(pred, true, w=0.5):
     error = torch.square(true - pred)
     
     weights = (torch.where((true != 0) & (pred == 0), 1, 0) * w) + 1
