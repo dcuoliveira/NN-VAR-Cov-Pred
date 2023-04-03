c1dd226a65c057e2eb7aafe6cdc545b1349c2fdc
dcuoliveira <dan.cunhaa@gmail.com>
2023-04-02 21:28:57 -0300

    simple nn

--- src/run_training.py
+++ src/run_training.py
@@ -1,20 +1,20 @@
 import os
-import torch
 import argparse
 
 from training.runners import run_new_model_training
 from settings import loss_metadata, model_metadata
 
-INPUTS_PATH = os.path.join(os.getcwd(), "data", "inputs")
-OUTPUTS_PATH = os.path.join(os.getcwd(), "data", "outputs")
-LOG_PATH = os.path.join(os.getcwd(), "data", "log")
+FILE_PATH = os.path.dirname(__file__)
+INPUTS_PATH = os.path.join(FILE_PATH, "data", "inputs")
+OUTPUTS_PATH = os.path.join(FILE_PATH, "data", "outputs")
+LOG_PATH = os.path.join(FILE_PATH, "data", "log")
 DATASET_NAMES = ["betadgp_corrdgp_data", "betadgp_covdgp_data", "betadgp_beta2x2_data", "betadgp_data"]
 TARGET_NAME = "betas_dgp"
 OUTPUT_OVRD = True
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n_jobs', type=float, default=-1, help='Dataset name.')
-parser.add_argument('--n_iter', type=int, default=100, help='Model name.')
+parser.add_argument('--n_jobs', type=float, default=1, help='Dataset name.')
+parser.add_argument('--n_iter', type=int, default=1, help='Model name.')
 parser.add_argument('--seed', type=int, default=2294, help='Model name.')
 parser.add_argument('--model_tag', type=str, default="mlp", help='Model name.')
 parser.add_argument('--standardize', type=bool, default=True, help='Model name.')
@@ -40,5 +40,5 @@ if __name__ == '__main__':
                                      seed=args.seed,
                                      verbose=args.verbose,
                                      output_ovrd=OUTPUT_OVRD,
-                                     dir_name_ovrd=["var_0.1_1_150", "var_0.1_1_200", "var_0.1_1_250", "var_0.1_1_300"],
+                                     dir_name_ovrd=["var_0.05_1_10"],
                                      classification=False)
